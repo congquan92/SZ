617060cba559f67fd49100caff5b7f5f55a221fc
congquan92 <145172432+congquan92@users.noreply.github.com>
2025-10-26 17:43:36 +0700

    init

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+    return (
+        <>
+            <h1>Hello World</h1>
+            <Button>Click Me</Button>
+        </>
+    );
+}
+
+export default App;
