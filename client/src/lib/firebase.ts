47d8ed88bfa574786a6e4d526cf077e436b5cf98
congquan92 <145172432+congquan92@users.noreply.github.com>
2025-11-21 12:01:38 +0700

    hehe

--- client/src/lib/firebase.ts
+++ client/src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD49lOlEc3jXvQurR--cW3hdVY0lwEzG4w",
-    authDomain: "ecommerce-d8b9c.firebaseapp.com",
-    databaseURL: "https://ecommerce-d8b9c-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "ecommerce-d8b9c",
-    storageBucket: "ecommerce-d8b9c.firebasestorage.app",
-    messagingSenderId: "149725130530",
-    appId: "1:149725130530:web:5f894a39ea6b58770f7f7b",
+    apiKey: `${import.meta.env.VITE_FIREBASE_API_KEY}`,
+    authDomain: `${import.meta.env.VITE_FIREBASE_AUTH_DOMAIN}`,
+    databaseURL: `${import.meta.env.VITE_FIREBASE_DATABASE_URL}`,
+    projectId: `${import.meta.env.VITE_FIREBASE_PROJECT_ID}`,
+    storageBucket: `${import.meta.env.VITE_FIREBASE_STORAGE_BUCKET}`,
+    messagingSenderId: `${import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID}`,
+    appId: `${import.meta.env.VITE_FIREBASE_APP_ID}`,
 };
 
 const app = initializeApp(firebaseConfig);
